199e5464994a7f4a1aa40e70535d557428204816
Baek-Taejun <126327414+Baek-Taejun@users.noreply.github.com>
2023-04-20 22:25:50 +0900

    nomad react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'; 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [amount, setAmount] = useState();
+  const [inverted,setInverted] = useState(false);
+  const onChange=(event)=>{
+    setAmount(event.target.value);
+  }
+
+  const reset = ()=> setAmount(0);
+  const onFlip = ()=>{
+      reset();
+      setInverted((current)=> !current);
+  }
+  return ( 
+    <div>
+        <h1 className="hi">Super Converter</h1> 
+
+        <div>
+        <label htmlFor="minutes">Minutes</label>
+        <input 
+        value={inverted ? amount * 60 : amount} 
+        id="minutes"  
+        placeholder= "Minutes" 
+        type = "number" 
+        disabled={inverted}
+        onChange={onChange}
+
+        /> 
+        </div>
+
+
+        <div>
+        <h4>You want to convert : {amount}</h4>
+        <label htmlFor="hours" >Hours</label>
+        <input 
+        value={inverted ? amount  : Math.round(amount / 60)} 
+        id="hours" 
+        placeholder= "Hours" 
+        type = "number" 
+        disabled={!inverted}
+        onChange={onChange}
+  
+        />
+        </div>
+
+        <button onClick={reset}>Reset</button>
+        <button onClick={onFlip}>Flip</button>
+
     </div>
-  );
+        );
 }
 
 export default App;
