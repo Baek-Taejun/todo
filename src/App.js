877eef9170ec13e478eeba88188a2e7d9c891f85
Baek-Taejun <126327414+Baek-Taejun@users.noreply.github.com>
2023-04-25 21:49:03 +0900

    230425

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import './App.css';
 import Button from "./Button";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
+function App() {   
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onClick = ()=> setCounter((prev)=> prev +1);
+  const onChange = (event) =>  setKeyword(event.target.value);
+  console.log("i run only once");
 
-function App() {  
+  useEffect(()=> {
+    console.log("i ren when keyword changes");
+  },[keyword]);
+
+  useEffect(()=> {
+    console.log("i run when counter changes");
+  },[keyword]);
+  
+
 
   return ( 
+
     <div>
-        <h1>hello </h1> 
-        <Button text={"continue"} />
+        <input
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder='Search here...'/>
+        <h1>{counter}</h1> 
+        <button onClick ={onClick}>Button</button>
     </div>
-        );
+  );  
   };  
 
 
 
-export default App;  
+export default App;
