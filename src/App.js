5314ff932424fedeea9f528c35dd3c66703a3755
Baek-Taejun <126327414+Baek-Taejun@users.noreply.github.com>
2023-04-25 13:00:22 +0900

    230425

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'; 
+import Button from "./Button";
 
-
-function Btn({text, onClick}){
-  return<button
-  onClick={onClick}
-    style={{
-    backgroundColor : "tomato",
-    color : "white",
-    padding : "10px 20px",
-    border : 0,
-    borderRadius : 10,
-
-  }}>{text}</button>
-}
 
 function App() {  
-  const [value,setValue]=useState("Save Changes");
-  const changeValue = () => setValue("Revert Change");
 
   return ( 
     <div>
-    <Btn text = {value} onClick={changeValue} />
-    <Btn text = "Continue" />
-
+        <h1>hello </h1> 
+        <Button text={"continue"} />
     </div>
         );
-  };
+  };  
 
 
 
-export default App; 
+export default App;  
